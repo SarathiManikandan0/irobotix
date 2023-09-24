9b974eef0af814a3de5e0c172364370f1d152991
SarathiManikandan0 <sarathimanikandan07@gmail.com>
2023-09-24 18:03:19 +0530

    Initial commit

---
+++ backend/data/products.js
@@ -0,0 +1,134 @@
+const products = [
+  {
+    name: "Arduino Braccio Robotic Arm",
+    image: "/images/arduino-braccio-robotic-arm.jpg",
+    description: `Description:
+      - Assemble your BRACCIO in a multitude of ways
+      - Designed for versatility
+      - Also support various objects on the end of the arm
+      - Included BRACCIO shield allows you to hook up the servos directly
+
+    Overview:
+      The Arduino Braccio Robotic Arm offers a world of possibilities with its versatile design. You can assemble it in various ways and even support different objects on the end of the arm. The included BRACCIO shield simplifies the connection of servos to your Arduino board.
+
+    Unlock the unlimited possibilities of robotics with Braccio! You can assemble your Braccio in a multitude of ways.`,
+    brand: "ARDUINOSKU: RB-Ard-81",
+    category: "Electronics",
+    price: 270.99,
+    countInStock: 3,
+    rating: 4.5,
+    numReviews: 4,
+  },
+  {
+    name: "Clearpath Robotics TurtleBot 4 Lite Mobile Robot",
+    image: "/images/clearpath-robotics-turtlebot.jpg",
+    description: `Description:
+      - TurtleBot 4 Lite Mobile Robot (assembled)
+      - Built on the iRobot Create 3 educational robot
+      - Features an Oak-D-Lite Camera
+      - Open-source robotics platform for Learning & Development
+
+    Overview:
+      The Clearpath Robotics TurtleBot 4 Lite Mobile Robot represents the next-generation robotic platform for learning the Robot Operating System (ROS). It builds on the success of previous versions by providing a low-cost and fully-extensible reference platform for robotics researchers, developers, and educators.
+
+    Key Features:
+      - Equipped with an iRobot® Create3 mobile base
+      - Powered by a Raspberry Pi 4 running ROS 2
+      - Includes 2D LiDAR and more
+      - Seamlessly integrated components for an out-of-the-box development and learning platform
+      - Features an OAK-D-Lite 4K RGB autofocus camera.`,
+    brand: "TB4 Lite",
+    category: "Electronics",
+    price: 1999.99,
+    countInStock: 10,
+    rating: 4.0,
+    numReviews: 4,
+  },
+  {
+    name: "Lynxmotion Smart Servo PRO Standard Modular Strain Wave Gear Actuator (LSS-P-S1)    ",
+    image: "/images/gear-actuator.jpg",
+    description: `Description:
+      - Lynxmotion Smart Servo PRO Standard (LSS-P-S1)
+      - Configurable, professional, modular smart servo motor
+      - Part of the Lynxmotion Servo Erector Set PRO (SES-PRO)
+      - 32Nm dynamic torque at 15rpm, 30V
+      - Strain wave gearing, STM32 Microcontroller, sensor feedback
+
+    Features:
+      - All metal strain wave gear coupled to a powerful NEMA 14 stepper motor offering high torque at low speeds.
+      - USB input and CAN BUS Communication
+      - Human-readable LSS Pro Communication Protocol (actions, queries, configurations)
+      - Daisy chained: RJ45 & XT60 
+      - Feedback: position, temperature, IMU, operating status & more
+      - 30V nominal input voltage
+      - STM32 microcontroller, stepper controller, and 16-bit magnetic encoder`,
+    brand: "LYNXMOTION",
+    category: "Electronics",
+    price: 929.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 3,
+  },
+  {
+    name: "RPLIDAR A2M8 360° Laser Scanner",
+    image: "/images/laser-scanner.jpg",
+    description: `Description:
+      - Low-Cost 360-Degree Laser Range Scanner
+      - Sample Frequency: 2000 - 8000 Hz
+      - Scan Rate: 5 - 15 Hz
+      - Distance Range: 0.15 - 12m
+      - Angular Resolution: 0.45 - 1.35°
+      - Barrel Connector Available (Small Barrel Connector Assembly 0.7x2.35 mm, Requires Soldering)
+
+    Overview:
+      The RPLIDAR A2M8 360° Laser Scanner represents the next generation of 360-degree 2D lidars. It utilizes a low-cost laser triangulation measurement system developed by SLAMTEC, resulting in excellent performance in various indoor and outdoor environments, even without direct sunlight exposure.
+
+    Key Features:
+      - Capable of taking up to 8000 samples of laser ranging per second due to its high rotation speed.
+      - Equipped with SLAMTEC's patented OPTMAG technology, extending the operational lifespan beyond traditional LIDAR systems for long-term stability.`,
+    brand: "SLAMTEC",
+    category: "Electronics",
+    price: 399.99,
+    countInStock: 10,
+    rating: 5,
+    numReviews: 3,
+  },
+  {
+    name: "Lynxmotion SES-V2 Standard Smart Servo (LSS-ST1)",
+    image: "/images/lynxmotion-smart-servo.jpg",
+    description: `Description:
+    - Lynxmotion SES-V2 Standard Smart Servo (LSS-ST1)
+    - Features a configurable, modular smart servo motor
+    - Designed for the Lynxmotion Servo Erector Set (SES)
+    - 60 RPM - no load (12V)
+    - 14 Kg-cm max static torque & 2.8 Kg-cm max dynamic torque (12V)
+    - Metal gears, RGB LED bar, sensor feedback, button interface
+    - Smart / wheel / RC modes
+    - Now with Full Aluminum Body (NEW)`,
+    brand: "LYNXMOTION",
+    category: "Electronics",
+    price: 72.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 2,
+  },
+  {
+    name: "Lynxmotion SES-PRO 550mm 6Dof Modular Robotic Arm Kit",
+    image: "/images/modular-robotic-arm-kit.jpg",
+    description: `Description:
+      - Modular, customizable & reconfigurable semi-professional arm
+      - Easy to use with human-readable communication protocol
+      - High accuracy and competitive payload capacity
+      - 550mm horizontal extension
+      - 2Kg payload and ±0.1 mm accuracy
+      - SES-PRO Arms estimated between $8,000 USD & $9,000 USD
+      - Gripper sold separately`,
+    brand: "LYNXMOTION",
+    category: "Electronics",
+    price: 9999.99,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 4,
+  },
+];
+export default products;
